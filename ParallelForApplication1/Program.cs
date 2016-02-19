43a93f6cc4a5cc447c114c4342b00614a21991e1
IZAQI <dbrmani@yahoo.co.jp>
2016-02-20 00:27:00 +0900

    range paritioner.

--- ParallelForApplication1/Program.cs
+++ ParallelForApplication1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,7 +12,8 @@ namespace ParallelForApplication1
     {
         static void Main(string[] args)
         {
-            new Program().Start();
+            //new Program().Start();
+            new Program().StartPartitioner();
 
             Thread.Sleep(5000);
         }
@@ -26,7 +28,29 @@ namespace ParallelForApplication1
                 array[i] = String.Format("{0} on thread {1}", i, Thread.CurrentThread.ManagedThreadId);
             });
 
+            //Parallel.ForEach(array, s =>
             foreach(var s in array) {
+            //{
+                System.Console.WriteLine(s);
+            }
+            //});
+        }
+        void StartPartitioner()
+        {
+            const int count = 100000;
+            var array = new String[count];
+
+            var rangePartitioner = Partitioner.Create(0, count);
+
+            Parallel.ForEach(rangePartitioner, (range, loopState)=>
+            {
+                for (int i = range.Item1; i < range.Item2; i++ )
+                    array[i] = String.Format("{0} on thread {1}", i, Thread.CurrentThread.ManagedThreadId);
+            });
+
+            foreach (var s in array)
+            {
+                //{
                 System.Console.WriteLine(s);
             }
         }
