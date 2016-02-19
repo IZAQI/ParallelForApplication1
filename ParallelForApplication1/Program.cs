90205e20c3b48d7910e2bfd39eb67621e001c000
IZAQI <dbrmani@yahoo.co.jp>
2016-02-19 22:33:48 +0900

    simple parallel for.

---
+++ ParallelForApplication1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelForApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+
+            Thread.Sleep(10000);
+        }
+
+        void Start()
+        {
+            const int count = 100000;
+            var array = new String[count];
+
+            Parallel.For(0, count, i =>
+            {
+                array[i] = Convert.ToString(i);
+            });
+
+            foreach(var s in array) {
+                System.Console.WriteLine(s);
+            }
+        }
+    }
+}
