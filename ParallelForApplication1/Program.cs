03d2bc77f3bea4400bca692566bb18e5873e5a58
IZAQI <dbrmani@yahoo.co.jp>
2016-02-20 01:07:42 +0900

    Time Compare.

--- ParallelForApplication1/Program.cs
+++ ParallelForApplication1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -12,47 +13,82 @@ namespace ParallelForApplication1
     {
         static void Main(string[] args)
         {
-            //new Program().Start();
-            new Program().StartPartitioner();
+
+            for (int i = 0; i < 5; i++)
+            {
+                new Program().Start();
+                new Program().StartPartitioner();
+                new Program().StartNormal();
+            }
 
             Thread.Sleep(5000);
         }
 
+        const int count = 500;
+
+        void StartNormal()
+        {
+            var array = new String[count];
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = String.Format("{0} on thread {1}", i, Thread.CurrentThread.ManagedThreadId);
+            }
+            stopWatch.Stop();
+
+            System.Console.WriteLine("Normal   calc time {0}s", stopWatch.Elapsed);
+        }
+
         void Start()
         {
-            const int count = 100000;
             var array = new String[count];
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
 
             Parallel.For(0, count, i =>
             {
                 array[i] = String.Format("{0} on thread {1}", i, Thread.CurrentThread.ManagedThreadId);
             });
+            stopWatch.Stop();
+
+            System.Console.WriteLine("Parallel calc time {0}s", stopWatch.Elapsed);
 
             //Parallel.ForEach(array, s =>
-            foreach(var s in array) {
+            //foreach(var s in array) {
             //{
-                System.Console.WriteLine(s);
-            }
+            //    System.Console.WriteLine(s);
+            //}
             //});
+
         }
         void StartPartitioner()
         {
-            const int count = 100000;
             var array = new String[count];
 
             var rangePartitioner = Partitioner.Create(0, count);
+
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
 
             Parallel.ForEach(rangePartitioner, (range, loopState)=>
             {
                 for (int i = range.Item1; i < range.Item2; i++ )
                     array[i] = String.Format("{0} on thread {1}", i, Thread.CurrentThread.ManagedThreadId);
             });
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            //TimeSpan ts = stopWatch.Elapsed;
 
-            foreach (var s in array)
-            {
-                //{
-                System.Console.WriteLine(s);
-            }
+            System.Console.WriteLine("Range    calc time {0}s", stopWatch.Elapsed);
+
+            //foreach (var s in array)
+            //{
+            //    System.Console.WriteLine(s);
+            //}
         }
     }
 }
